dccb2ab7e6a691da02b7bbadfa723ee441425bc1
Ashypookiebear <Remsquare@gmail.com>
2025-07-27 02:41:31 -0700

    added an error message that warns the player that they need to use a number value in their command for /sensory. added a new HashSet for warnedPlayers so it doesnt spam console/chat when players continue to type their silly ahh alphabet values

--- src/main/java/me/remsq/gaidenUtils/tabcompletors/SensoryTabCompleter.java
+++ src/main/java/me/remsq/gaidenUtils/tabcompletors/SensoryTabCompleter.java
@@ -1,20 +1,35 @@
 package me.remsq.gaidenUtils.tabcompletors;
 
 
-import me.remsq.gaidenUtils.GaidenUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 
 public class SensoryTabCompleter implements TabCompleter {
+    private final Set<UUID>  warnedPlayers = new HashSet<>();
+
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 1) {
+            if (!args[0].matches("\\d*")) {
+                if (sender instanceof org.bukkit.entity.Player player) {
+                    if (!warnedPlayers.contains(player.getUniqueId())) {
+                        sender.sendMessage(ChatColor.RED + "You must type a number value.");
+                        warnedPlayers.add(player.getUniqueId());
+                    }
+                }
+                return Collections.emptyList();
+            } else {
+                if (sender instanceof org.bukkit.entity.Player player) {
+                    warnedPlayers.remove(player.getUniqueId());
+                }
+            }
 
+        }
         return Collections.emptyList();
     }
 }
